78adcab2cada333ce8be7fabe4efb14d013e1e55
ThotaSairam123 <thotasairam999@gmail.com>
2023-01-19 16:19:54 +0530

    changes made by sairam

--- DeletePlugin/DeletePlugin/FunctionUpdate.cs
+++ DeletePlugin/DeletePlugin/FunctionUpdate.cs
@@ -26,6 +26,10 @@ namespace DeletePlugin
         {
             int z = x-y;
         }
+        public void ThotaSairam(int x, int y,int z)
+        {
+            int z = x/y;
+        }
         
  
         public void Kamala(IServiceProvider serviceProvider){
